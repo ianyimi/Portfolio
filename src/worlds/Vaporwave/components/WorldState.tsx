208638fbaf34b63054e4c7a4bafa93bcc37ff3a7
ianyimi <anyiminene@gmail.com>
2022-06-16 21:43:40 -0700

    cookout playlist added

--- src/worlds/Vaporwave/components/WorldState.tsx
+++ src/worlds/Vaporwave/components/WorldState.tsx
@@ -85,7 +85,7 @@ function startPlaylist() {
 
 	};
 
-	const zeroPlaylist = playlists[ 1 ];
+	const zeroPlaylist = playlists[ 0 ];
 	const firstPlaylist = {
 		...zeroPlaylist,
 		palette: randomItem( zeroPlaylist.palettes )
