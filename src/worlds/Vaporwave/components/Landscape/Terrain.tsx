ea378e862e0e9d3dff0b6802654e261f0c646f60
ianyimi <anyiminene@gmail.com>
2022-06-29 15:23:22 -0700

    man wtf

--- src/worlds/Vaporwave/components/Landscape/Terrain.tsx
+++ src/worlds/Vaporwave/components/Landscape/Terrain.tsx
@@ -1,5 +1,5 @@
 import { useTexture } from "@react-three/drei";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { animated, useSpring } from "react-spring/three";
 import MatrixSky from "./MatrixSky";
 import * as THREE from "three";
@@ -7,13 +7,40 @@ import AmbientParticles from "./AmbientParticles";
 import AudioVisualizer from "./AudioVisualizer";
 import { useStore } from "utils/store";
 import shallow from "zustand/shallow";
+import { GroupProps, useThree } from "@react-three/fiber";
 
-const Terrain = React.forwardRef( ( props: { z?: number }, ref ) => {
+const useForwardedRef = ( ref: React.ForwardedRef<unknown> ) => {
 
-	const { z = 0 } = props;
+	const innerRef = useRef<unknown>( null );
+
+	useEffect( () => {
+
+		if ( ! ref ) return;
+		if ( typeof ref === "function" ) {
+
+			ref( innerRef.current );
+
+		} else {
+
+			innerRef.current = ref.current;
+
+		}
+
+	}, [ ref ] );
+
+	return innerRef;
+
+};
+
+const Terrain = React.forwardRef( ( props: { z?: number } & GroupProps, ref ) => {
+
+	const { z = 0, ...restProps } = props;
 	const { playlist } = useStore( ( state ) => ( {
 		playlist: state.playlist,
 	} ), shallow );
+	const { gl } = useThree();
+	const safeRef = useForwardedRef( ref );
+	// console.log( safeRef.current );
 
 	const [ heightTexture, metalnessTexture ] = useTexture( [
 		"displacement-7.png",
@@ -28,59 +55,98 @@ const Terrain = React.forwardRef( ( props: { z?: number }, ref ) => {
 			mass: 1
 		}
 	} );
+	// useEffect( () => {
+	//
+	// 	const onChange = () => {
+	//
+	// 		// @ts-ignore
+	// 		// if ( ! ref || ! ref.current ) return;
+	//
+	// 		console.log( "reset landscape" );
+	//
+	// 		// @ts-ignore
+	// 		// t1Pos.current = 0;
+	// 		// @ts-ignore
+	// 		// t2Pos.current = 4;
+	//
+	// 		// if ( document.hidden ) {
+	//
+	// 		gl.getObjectByName();
+	// 		// safeRef.current.position.set( 0, 0, z );
+	// 		console.log( safeRef.current );
+	//
+	// 		// } else {
+	// 		//
+	// 		// 	setRender( false );
+	// 		//
+	// 		// }
+	//
+	// 	};
+	//
+	// 	document.addEventListener( "visibilitychange", onChange );
+	//
+	// 	return () => {
+	//
+	// 		document.removeEventListener( "visibilitychange", onChange );
+	//
+	// 	};
+	//
+	// }, [] );
 
 	return (
-		// @ts-ignore
-		<group ref={ref} position={[ 0, 0, z ]}>
-			<MatrixSky/>
-			<AmbientParticles/>
-			<AudioVisualizer
-				position={[ 0, 0, 0 ]}
-				radius={0.5}
-				barCount={45}
-				index={1}
-			/>
-			<AudioVisualizer
-				position={[ 0, 0, - 2 ]}
-				radius={0.5}
-				barCount={45}
-				index={1}
-				reverse
-			/>
-			<group rotation={[ - Math.PI * 0.5, 0, 0 ]}>
-				<mesh>
-					<animated.meshStandardMaterial
-						color={lineColor}
-						emissive={lineColor}
-						displacementMap={heightTexture}
-						displacementScale={0.4}
-						metalnessMap={metalnessTexture}
-						metalness={0.9}
-						roughness={0.5}
-						wireframe
-					/>
-					<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
-				</mesh>
-				<mesh>
-					<animated.meshStandardMaterial
-						color="black"
-						displacementMap={heightTexture}
-						displacementScale={0.3975}
-						metalnessMap={metalnessTexture}
-						metalness={0.9}
-						roughness={0.5}
-					/>
-					<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
-				</mesh>
-				<mesh position-y={- 0.1}>
-					<animated.meshStandardMaterial
-						color="black"
-						metalnessMap={metalnessTexture}
-						metalness={0.9}
-						roughness={0.5}
-					/>
-					<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
-				</mesh>
+	// @ts-ignore
+		<group ref={ref} position={[ 0, 0, z ]} {...restProps}>
+			<group>
+				<MatrixSky/>
+				<AmbientParticles/>
+				<AudioVisualizer
+					position={[ 0, 0, 0 ]}
+					radius={0.5}
+					barCount={45}
+					index={1}
+				/>
+				<AudioVisualizer
+					position={[ 0, 0, - 2 ]}
+					radius={0.5}
+					barCount={45}
+					index={1}
+					reverse
+				/>
+				<group rotation={[ - Math.PI * 0.5, 0, 0 ]}>
+					<mesh>
+						<animated.meshStandardMaterial
+							color={lineColor}
+							emissive={lineColor}
+							displacementMap={heightTexture}
+							displacementScale={0.4}
+							metalnessMap={metalnessTexture}
+							metalness={0.9}
+							roughness={0.5}
+							wireframe
+						/>
+						<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
+					</mesh>
+					<mesh>
+						<animated.meshStandardMaterial
+							color="black"
+							displacementMap={heightTexture}
+							displacementScale={0.3975}
+							metalnessMap={metalnessTexture}
+							metalness={0.9}
+							roughness={0.5}
+						/>
+						<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
+					</mesh>
+					<mesh position-y={- 0.1}>
+						<animated.meshStandardMaterial
+							color="black"
+							metalnessMap={metalnessTexture}
+							metalness={0.9}
+							roughness={0.5}
+						/>
+						<planeBufferGeometry attach="geometry" args={[ 1, 4, 24, 24 ]}/>
+					</mesh>
+				</group>
 			</group>
 		</group>
 	);
--- src/worlds/Vaporwave/components/Landscape/index.tsx
+++ src/worlds/Vaporwave/components/Landscape/index.tsx
@@ -1,6 +1,6 @@
-import { Fog, useLimiter } from "spacesvr";
+import { useLimiter } from "spacesvr";
 import { useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Terrain from "./Terrain";
 import { usePlane } from "@react-three/cannon";
 import Audio from "../Audio";
@@ -24,9 +24,12 @@ const BALL_START_POSITIONS: [x: number, y: number, z: number][] = [
 	[ - 0.05, 0.075, - 2.5 ],
 	[ - 0.1, 0.125, - 3.25 ]
 ];
+const BALL_SPEED_FACTOR = 0.0015;
+const BALL_RESPAWN_POINT = 0.0015;
 
 export default function Index() {
 
+	const landscape = useRef();
 	const terrain1Ref = useRef();
 	const terrain2Ref = useRef();
 	const terrainRefs = [ terrain1Ref, terrain2Ref ];
@@ -35,6 +38,9 @@ export default function Index() {
 	const ball2 = useRef();
 	const ball3 = useRef();
 	const ballRefs = [ ball, ball1, ball2, ball3 ];
+	const sleep = useRef( false );
+
+	const [ render, setRender ] = useState( false );
 
 	const tex = useTexture( TEXTURES[ 0 ] );
 	const tex2 = useTexture( TEXTURES[ 1 ] );
@@ -52,51 +58,139 @@ export default function Index() {
 		type: "Static"
 	} ) );
 
-	const ballSpeedFactor = 0.0015;
-	const ballRespawnPoint = 1.15;
+	const t1Pos = useRef( 0 );
+	const t2Pos = useRef( - 4 );
+
+	useEffect( () => {
+
+		const onChange = () => {
+
+			if ( ! landscape.current || ! terrain1Ref.current || ! terrain2Ref.current ) return;
+
+			console.log( "reset landscape" );
+
+			if ( document.hidden ) {
+
+				sleep.current = true;
+				// @ts-ignore
+				landscape.current.getObjectByName( "terrain1" ).position.set( 0, 0, 0 );
+				t1Pos.current = 0;
+				// @ts-ignore
+				landscape.current.getObjectByName( "terrain2" ).position.set( 0, 0, - 4 );
+				t2Pos.current = - 4;
+
+			} else {
+
+				sleep.current = false;
+
+			}
+
+			// @ts-ignore
+			// landscape.current.getObjectByName( "terrain1" ).position.set( 0, 0, 0 );
+			// // @ts-ignore
+			// landscape.current.getObjectByName( "terrain2" ).position.set( 0, 0, - 4 );
+
+			// @ts-ignore
+			// t1Pos.current = 0;
+			// @ts-ignore
+			// t2Pos.current = 4;
+
+
+			// if ( document.hidden ) {
+			//
+			// 	setRender( true );
+			//
+			// } else {
+			//
+			// 	setRender( false );
+			//
+			// }
+			console.log( terrain1Ref.current.position.z );
+			// @ts-ignore
+			// console.log( terrain2Ref.current.position.z );
+
+		};
+
+		document.addEventListener( "visibilitychange", onChange );
+
+		return () => {
+
+			document.removeEventListener( "visibilitychange", onChange );
+
+		};
+
+	}, [] );
 
 	const limiter = useLimiter( 30 );
 	useFrame( ( { clock }, delta ) => {
 
 		if (
 			! limiter.isReady ||
-			! terrain1Ref.current ||
-			! terrain2Ref.current ||
-			! ball.current ||
-			! ball1.current ||
-			! ball2.current ||
-			! ball3.current
+      sleep.current ||
+      ! terrain1Ref.current ||
+      ! terrain2Ref.current ||
+      ! ball.current ||
+      ! ball1.current ||
+      ! ball2.current ||
+      ! ball3.current
 		) return;
 
 		const speed = getSpeed();
 
-		for ( const terrain of terrainRefs ) {
+		// @ts-ignore
+		t1Pos.current += delta / ( 5 * speed );
+		t2Pos.current += delta / ( 5 * speed );
 
-			// @ts-ignore
-			terrain.current.position.z += delta / ( 5 * speed );
+		// console.log( terrain1Ref.current.position.z );
 
-			// @ts-ignore
-			if ( terrain.current.position.z >= 3.5 ) {
+		// @ts-ignore
+		terrain1Ref.current.position.set( 0, 0, t1Pos.current );
 
-				// @ts-ignore
-				terrain.current.position.z = - 4.5;
+		// @ts-ignore
+		terrain2Ref.current.position.set( 0, 0, t2Pos.current );
 
-			}
+		if ( t1Pos.current >= 3.5 ) {
+
+			t1Pos.current = - 4.5;
 
 		}
+
+		if ( t2Pos.current >= 3.5 ) {
+
+			t2Pos.current = - 4.5;
+
+		}
+
+
+		// for ( const terrain of terrainRefs ) {
+		//
+		// 	//
+		// 	// 	// @ts-ignore
+		// 	// 	terrain.current.position.z += delta / ( 5 * speed );
+		// 	//
+		// 	// @ts-ignore
+		// 	if ( terrain.current.position.z >= 3.5 ) {
+		//
+		// 		// @ts-ignore
+		// 		terrain.current.position.z = - 4.5;
+		//
+		// 	}
+		// 	//
+		//
+		// }
 
 		for ( let i = 0; i < ballRefs.length; i ++ ) {
 
 			const ball = ballRefs[ i ];
 
 			// @ts-ignore
-			ball.current.position.z += delta / ( 5 * speed ) - ballSpeedFactor;
+			ball.current.position.z += delta / ( 5 * speed ) - BALL_SPEED_FACTOR;
 
 			// @ts-ignore
-			if ( ball.current.position.z + BALL_START_POSITIONS[ i ][ 2 ] >= ballRespawnPoint ) {
+			if ( ball.current.position.z + BALL_START_POSITIONS[ i ][ 2 ] >= BALL_RESPAWN_POINT ) {
 
 				// @ts-ignore
-				ball.current.position.z = BALL_START_POSITIONS[ i ][ 2 ] + ballRespawnPoint;
+				ball.current.position.z = BALL_START_POSITIONS[ i ][ 2 ] + BALL_RESPAWN_POINT;
 
 			}
 
@@ -105,13 +199,13 @@ export default function Index() {
 	} );
 
 	return (
-		<group>
+		<group ref={landscape}>
 			<Audio fftSize={2048}/>
-			<Fog color={new THREE.Color( playlist.palette[ playlist.backgroundColorIndex ] )} near={1} far={2}/>
+			{/*<Fog color={new THREE.Color( playlist.palette[ playlist.backgroundColorIndex ] )} near={1} far={2}/>*/}
 			<Lights/>
 			{/*<Title position={[0, 0.5, -0.5]} />*/}
-			<Terrain ref={terrain1Ref}/>
-			<Terrain z={- 4} ref={terrain2Ref}/>
+			<Terrain ref={terrain1Ref} name="terrain1"/>
+			<Terrain z={- 4} ref={terrain2Ref} name="terrain2"/>
 			<Ball
 				ref={ball}
 				pos={BALL_START_POSITIONS[ 0 ]}
