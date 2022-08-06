f9b9bc0bd8530c3aadbe19d425a293f46491f75c
ianyimi <anyiminene@gmail.com>
2022-08-05 17:16:32 -0700

    added title text in 3d

--- src/worlds/InfiniteScroll/components/Scene/components/SectionOne/components/TitleText.tsx
+++ src/worlds/InfiniteScroll/components/Scene/components/SectionOne/components/TitleText.tsx
@@ -1,5 +1,5 @@
 import { GroupProps, useFrame, useThree } from "@react-three/fiber";
-import { Float, Text3D, useScroll } from "@react-three/drei";
+import { Float, Html, Text3D, useScroll } from "@react-three/drei";
 import { useRef, useState } from "react";
 import * as THREE from "three";
 import { animated, useSpring } from "react-spring/three";
@@ -16,13 +16,13 @@ export default function TitleText( props: GroupProps ) {
 	const data = useScroll();
 
 	const { z } = useSpring( {
-		z: enter && active ? 0 : - 5,
+		z: enter && active ? 0 : - 1,
 		config: {
-			duration: active ? 2000 : 500,
+			duration: active ? 1000 : 2000,
 			mass: 1,
 			tension: 180,
 			friction: 12,
-			easing: active ? easings.easeElastic : easings.easeBackOut
+			easing: active ? easings.easeSinIn : easings.easeBackOut
 		}
 	} );
 
@@ -31,13 +31,12 @@ export default function TitleText( props: GroupProps ) {
 
 		if ( ! limiter.isReady( clock ) ) return;
 
-		const ready = data.range( 1 / 3, 2 / 3 );
-		console.log( ready );
-		if ( ready <= 0 && ! active ) {
+		const ready = data.range( 0, 1 / 3 );
+		if ( ready < 1 && ! active ) {
 
 			setActive( true );
 
-		} else if ( active ) {
+		} else if ( ready >= 1 && active ) {
 
 			setActive( false );
 
@@ -45,6 +44,8 @@ export default function TitleText( props: GroupProps ) {
 
 
 	} );
+
+	console.log( active );
 
 	return (
 		<group {...props}>
@@ -54,12 +55,25 @@ export default function TitleText( props: GroupProps ) {
 			{/*</mesh>*/}
 			<animated.group position-z={z}>
 				<Float speed={2} floatIntensity={0.5} floatingRange={[ 0, 0.0005 ]} position={[ camera.position.x, - 0.05, 0 ]}>
+					{/* @ts-ignore */}
 					<Text3D size={0.025} height={0.0075} font="https://dqeczc7c9n9n1.cloudfront.net/fonts/bitmap.json">
 						Isaiah Anyimi
 						<meshStandardMaterial emissive="white" roughness={0.8} metalness={0.2} side={THREE.DoubleSide}/>
 					</Text3D>
 				</Float>
 			</animated.group>
+			{/*<Html transform>*/}
+			{/*	<h1*/}
+			{/*		style={{*/}
+			{/*			color: "white",*/}
+			{/*			fontFamily: "Thunderstorm",*/}
+			{/*			fontSize: "10em",*/}
+			{/*			width: "80vw"*/}
+			{/*		}}*/}
+			{/*	>*/}
+			{/*		Isaiah Anyimi*/}
+			{/*	</h1>*/}
+			{/*</Html>*/}
 		</group>
 	);
 
