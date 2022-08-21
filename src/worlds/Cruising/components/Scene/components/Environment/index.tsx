22227cfb261d05233fb5c255552591abe76e2dc3
ianyimi <anyiminene@gmail.com>
2022-08-20 22:35:41 -0700

    new car with turnin wheels

--- src/worlds/Cruising/components/Scene/components/Environment/index.tsx
+++ src/worlds/Cruising/components/Scene/components/Environment/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@react-three/drei";
 import SL190 from "./models/Sl190";
+import SL1902 from "./models/Sl1902";
 import { Suspense } from "react";
 
 export default function Environment() {
@@ -8,7 +9,8 @@ export default function Environment() {
 		<group>
 			<ambientLight intensity={0.5} />
 			<Suspense fallback={null}>
-				<SL190 />
+				{/*<SL190 />*/}
+				<SL1902 />
 			</Suspense>
 			<Box args={[ 1, 1, 1 ]} >
 				<meshBasicMaterial color="red" />
