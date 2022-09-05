c0a7b43af476d3fbd023201a61fb34e7b6d9669e
ianyimi <anyiminene@gmail.com>
2022-09-05 13:26:49 -0700

    tiny file changes for linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic'
 import Instructions from '@/components/dom/Instructions'
 // import Shader from '@/components/canvas/Shader/Shader'
 
-// Dynamic import is used to prevent a payload when the website start that will include threejs r3f etc..
+// Dynamic import is used to prevent a payload when the website start that will include three.js r3f etc...
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
