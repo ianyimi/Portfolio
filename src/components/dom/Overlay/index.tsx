466060e3a505a527c87392386ff6c063f8b5eefe
ianyimi <anyiminene@gmail.com>
2022-09-19 13:58:58 -0700

    styles for no mobile

--- src/components/dom/Overlay/index.tsx
+++ src/components/dom/Overlay/index.tsx
@@ -1,9 +1,10 @@
 import {Loader} from "@react-three/drei";
 import {isMobile} from "react-device-detect"
+import styles from "./index.module.css";
 
 export default function Overlay() {
   return (
-    isMobile ? <div>
+    isMobile ? <div className={styles.mobile}>
       Mobile under Development! Reconnect on desktop for the experience
     </div> : <div>
       <Loader dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}/>
