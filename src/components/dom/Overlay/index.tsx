7fc75b661da073bb49b6b54cd27571fa4f1850b4
ianyimi <anyiminene@gmail.com>
2022-09-19 14:09:41 -0700

    styles for no mobile again

--- src/components/dom/Overlay/index.tsx
+++ src/components/dom/Overlay/index.tsx
@@ -5,7 +5,7 @@ import styles from "./index.module.css";
 export default function Overlay() {
   return (
     isMobile ? <div className={styles.mobile}>
-      Mobile under Development! Reconnect on desktop for the experience
+      Mobile under Development! Please reconnect on a desktop or tablet.
     </div> : <div>
       <Loader dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}/>
     </div>
