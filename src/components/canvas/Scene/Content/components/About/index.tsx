054a37f2c4fe501a64360fea7500a5551e05e05c
ianyimi <anyiminene@gmail.com>
2022-09-20 15:34:56 -0700

    about slide text changes

--- src/components/canvas/Scene/Content/components/About/index.tsx
+++ src/components/canvas/Scene/Content/components/About/index.tsx
@@ -8,50 +8,50 @@ import Interest from "./Interest";
 import ProfileCard from "./ProfileCard";
 
 export default function About(props: { viewHelpers?: boolean }) {
-  
+
   const {viewHelpers = false} = props;
-  
+
   const {currentSection, previousSection, animating} = useStore(state => ({
     currentSection: state.currentSection,
     previousSection: state.previousSection,
     animating: state.animating,
   }));
   const active = currentSection && currentSection.name === "About";
-  
+
   const interests = [];
   for (let i = 0; i < INTERESTS.length; i++) {
     interests.push(
       <Interest data={INTERESTS[i]} index={i} key={i}/>
     )
   }
-  
+
   const groupAnimate = {
     x: 1,
     y: active || (previousSection && previousSection.name === "About" && animating) ? 1.2 : 3,
     z: -0.25
   };
-  
+
   const profileAnimate = {
     scale: active && !animating ? 1 : 0
   }
-  
+
   const bioImageAnimate = {
     y: active && !animating ? 0 : "-30px",
     opacity: active && !animating ? 1 : 0
   }
-  
+
   const bioSummaryAnimate = {
     y: active && !animating ? 0 : "30px",
     opacity: active && !animating ? 1 : 0
   }
-  
+
   const basicTransition = {
     delay: active ? 0.3 : 0,
     duration: active ? 0.75 : 0.25,
     when: "beforeChildren",
     staggerChildren: 0.5
   }
-  
+
   return (
     <group>
       <Motion.group animate={groupAnimate}>
@@ -67,12 +67,13 @@ export default function About(props: { viewHelpers?: boolean }) {
               transition={basicTransition}
             >
               <div className={styles.cardContent}>
-                About Me Summary
+                I am Isaiah Anyimi. Not just any react developer, here are some other things I often partake in while
+                not building something new.
               </div>
             </motion.div>
           </Html>
         </group>
-      
+
       </Motion.group>
     </group>
   )
