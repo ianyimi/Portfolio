a66dbec5e80147a4654c8feb293204e34bf9c45c
ianyimi <anyiminene@gmail.com>
2022-10-31 16:11:46 -0700

    mobile redirect to carrds completed

--- src/components/canvas/Scene/index.tsx
+++ src/components/canvas/Scene/index.tsx
@@ -1,16 +1,9 @@
 import {GroupProps} from "@react-three/fiber";
 import Camera from "./components/Camera";
-import Loader from "./components/Loader";
 import Content from "./Content";
 import Environment from "./Environment";
-import {useEffect} from "react";
-import {isMobile} from "react-device-detect";
 
 export default function Scene(props: GroupProps) {
-  
-  // useEffect(() => {
-  //   if (isMobile) window.location.replace("https://ianyimi.carrd.co")
-  // }, [])
   
   return (
     <group {...props}>
