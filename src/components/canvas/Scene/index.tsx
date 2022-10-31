f18e9942a9d095e8f6270b783a917d004afffb09
ianyimi <anyiminene@gmail.com>
2022-10-31 16:06:40 -0700

    mobile redirect to carrd try 4

--- src/components/canvas/Scene/index.tsx
+++ src/components/canvas/Scene/index.tsx
@@ -9,7 +9,7 @@ import {isMobile} from "react-device-detect";
 export default function Scene(props: GroupProps) {
   
   useEffect(() => {
-    if (isMobile) window.open("ianyimi.carrd.co")
+    if (isMobile) window.open("ianyimi.carrd.co", "_self")
   }, [])
   
   return (
