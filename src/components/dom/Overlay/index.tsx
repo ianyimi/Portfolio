0ed11a6ad2443c309a17def0d46476c6f0f5a1e0
ianyimi <anyiminene@gmail.com>
2022-10-31 14:09:23 -0700

    camera deleted comments and mobile redirect to carrd

--- src/components/dom/Overlay/index.tsx
+++ src/components/dom/Overlay/index.tsx
@@ -4,13 +4,10 @@ import {isMobile} from "react-device-detect"
 import styles from "./index.module.css";
 
 export default function Overlay() {
+  if (isMobile) window.open("https://ianyimi.carrd.co");
   return (
-    // isMobile ? <div className={styles.mobile}>
-    //   Mobile under Development! Please reconnect on a desktop or tablet.
-    // </div> :
     <div>
       <Loader dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}/>
-      {/*<LoadingScreen/>*/}
     </div>
   )
 }
